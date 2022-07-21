95496503b11af90ba06b2ae451079acbd6b3fa01
Said Hussein <saidhussein12342@gmail.com>
2022-07-21 20:48:12 +0300

    commit tree 66dbdcaa30ac037d9bc89f30ebf3b91c828098eb

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import { UserProvider } from "../context/user"
+
+function UserProvider({ children }) {
+  const currentUser = {
+    name: "Duane",
+    interests: ["Coding", "Biking", "Words ending in 'ing'"],
+  };
+  return (
+    <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>
+  );
+}
 
 function App() {
+
   const [theme, setTheme] = useState("dark");
-  const [user, setUser] = useState(null);
+
   return (
     <main className={theme}>
+      <UserProvider>
       <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
       <Profile theme={theme} user={user} />
+    </UserProvider>
     </main>
   );
 }
---
+++ src/context/user.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import user from "../data";
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    return <UserContext.Provider value={ {user, setUser}}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
